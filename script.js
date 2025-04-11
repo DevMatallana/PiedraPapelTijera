1b1cfb8d8c0ba75d9a8b373f12dae860989037cb
DevMatallana <duanmatallanaxd@hotmail.com>
2025-04-11 13:55:54 -0500

    Agregado ManipulationDom con JavaScript

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
+const PAPEL = "PAPEL"
+const PIEDRA = "PIEDRA"
+const TIJERA = "TIJERA"
+
+const $ = el => document.querySelector(el)
+
+const btnPiedra = $(".Piedra") 
+const btnPapel = $(".Papel")
+const btnTijera = $(".Tijera")
+const btnPLayGame = $(".PlayGame")
+const parrafo = $(".parra")
+const parraComputer = $(".parraOne")
+const parraWin = $(".parraTwo")
+const parraPuntaje = $(".parraThree")
+const parraELement = $(".parraFour")
+
+let estado = "";
+let humanScore = 0
+let ComputerScore = 0
 
 
 function getComputerChoice() {
-    let computerCalculo = Math.floor(Math.random() * 4);
-    if (computerCalculo === 1) {
-        return "PIEDRA"
+    let computerCalculo = Math.floor(Math.random() * 3);
+    if (computerCalculo === 0) {
+        return PIEDRA
+    } else if (computerCalculo === 1) {
+        return PAPEL
     } else if (computerCalculo === 2) {
-        return "PAPEL"
-    } else if (computerCalculo === 3) {
-        return "TIJERA"
+        return TIJERA
     }
-    return "none"
-}
+} 
 
-function getHumanChoice() {
-    return prompt("Piedra, Papel o Tijera");
-}
+btnPiedra.addEventListener("click", () => {
+    parrafo.textContent = "Piedra";
+    estado = PIEDRA;
+});
+
+btnPapel.addEventListener("click", () => {
+    parrafo.textContent = "Papel";
+    estado = PAPEL;
+});
+
+btnTijera.addEventListener("click", () => {
+    parrafo.textContent = "Tijeras";
+    estado = TIJERA;
+});
+
+btnPLayGame.addEventListener("click", () => {
+    playGame()
+})
+
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toUpperCase()
     if (humanChoice === computerChoice) {
         return 1
     } else if (humanChoice === "PAPEL" && computerChoice === "PIEDRA") {
@@ -31,34 +64,40 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-function playGame() {        
-    let humanScore = 0
-    let ComputerScore = 0
-    let i = 0
+// Inicia el juego
 
-    while (i <= 5) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
+function playGame() {
+    
+    const humanSelection = estado;
+    const computerSelection = getComputerChoice();
+    
+    if (humanSelection === "") {
+        alert("No has seleccionado ningun elemento")
+        return "none"
+    }
+    parraComputer.textContent = computerSelection;
+    let round = playRound(humanSelection, computerSelection)
         
-        let game = playRound(humanSelection, computerSelection)
-        if (game === 1, 2, 3, 4, 5 && game !== 5) {
-            humanScore += 1
-            console.log("Has ganado!")
-        } else {
-            ComputerScore += 1
-            console.log("Has perdido")
-        }
-        
-        i++
+    if (round === 1) {
+        parraWin.textContent = "Empate";
+    } else if (round === 2, 3, 4 && round !== 5) {
+        parraWin.textContent = "Has ganado!";
+        humanScore += 1
+    } else if (round === 5) {
+        parraWin.textContent = "Has perdido :(";
+        ComputerScore += 1
     }
     
-    if (humanScore > ComputerScore) {
-        return "Le has ganado a la maquina con " + humanScore + " Puntos!"
-    } else {
-        return "La maquina te ah ganado con " + ComputerScore + "Puntos :(" 
+    if (humanScore >= 5) {
+        alert("Has ganado!");
+        humanScore = 0;
+        ComputerScore = 0;
+    } else if (ComputerScore >= 5) {
+        alert("Has perdido :(");
+        humanScore = 0;
+        ComputerScore = 0;
     }
 
+    parraPuntaje.textContent = humanScore;
+    parraELement.textContent = ComputerScore;
 }
-
-console.log(playGame())
-
